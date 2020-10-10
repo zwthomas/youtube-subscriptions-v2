b5249bacff527696eff0ba6b6d73bf888b36f086
Craven <zwthomas13@gmail.com>
2020-10-10 07:44:54 -0500

    switching machines

--- youtubeSetup.py
+++ youtubeSetup.py
@@ -1,9 +1,9 @@
 import sqlite3
-import google_auth_oauthlib.flow
-import googleapiclient.errors
-import googleapiclient.discovery
 import hvac
 import time
+import logging
+from bs4 import BeautifulSoup
+
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -18,14 +18,21 @@ class YoutubeSetup():
     URL = "https://www.youtube.com/channel/{}/channels"
 
     def __init__(self):
+        logging.basicConfig()
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.DEBUG)
+
         vaultClient = hvac.Client(
             url="http://192.168.73.20:8200",
             token="s.tLWbbS9mBlEcDedkystiYG8P"
         )
         self.channel = vaultClient.secrets.kv.read_secret_version(path="youtube")["data"]["data"]["channel"]
-        print(self.channel)
 
+        
+        
     def run(self):
+        self.logger.info("Init Driver")
+
         chrome_options = Options()
         # chrome_options.add_argument("--headless")
         # chrome_options.add_argument("--no-sandbox")
@@ -42,13 +49,40 @@ class YoutubeSetup():
         self.getChannels(driver)
     
     def getChannels(self, driver):
+        self.logger.info("Getting Channel Data")
         # driver.findElement(By.cssSelector("body")).sendKeys(Keys.CONTROL, Keys.END);
         WebDriverWait(driver, 10).until( EC.presence_of_element_located((By.ID, "channel")))
+        self.loadAllSubs(driver)
+        links = self.getChannelLinks(driver)
+        print(links)
+        # channels = driver.find_elements_by_id("channel")
+        # for channel in channels:
+        #     soup = BeautifulSoup(channel.get_attribute("innerHTML"), 'html.parser')
+        #     channelLink = soup.find("a")["href"]
+            
+        #     print(channelLink)
 
-        driver.find_element_by_css_selector("body").send_keys(Keys.CONTROL, Keys.END)
-        time.sleep(2)
+
+    def loadAllSubs(self, driver):
+        self.logger.info("Loading All Subs")
+        prevNum = 0
+        numChannels = len(driver.find_elements_by_id("channel"))
+
+        while(prevNum < numChannels):
+            prevNum = numChannels
+            driver.find_element_by_css_selector("body").send_keys(Keys.CONTROL, Keys.END)
+            time.sleep(2)
+            numChannels = len(driver.find_elements_by_id("channel"))
+    def getChannelLinks(self, driver):
+        links = []
         channels = driver.find_elements_by_id("channel")
-        print(len(channels))
+        for channel in channels:
+            soup = BeautifulSoup(channel.get_attribute("innerHTML"), 'html.parser')
+            channelLink = soup.find("a")["href"]
+            name = soup.find("span", {"id":"title"}).contents[0]
+            print(name)
+            links.append(channelLink)
+        return links
 
 if __name__ == "__main__":
     ytSetup = YoutubeSetup()
