44c211d60e0dd6a8ff388e16d6c638edff63afa9
Zach Thomas <zwthomas13@gmail.com>
2021-05-04 20:14:04 -0500

    updating data

--- note.md
+++ note.md
@@ -18,3 +18,89 @@ id
 </paper-tooltip>
 </div>
 ```
+
+
+```
+{
+    "id": "yt:video:bNwWDifV8Bs",
+    "guidislink": True,
+    "link": "https://www.youtube.com/watch?v=bNwWDifV8Bs",
+    "yt_videoid": "bNwWDifV8Bs",
+    "yt_channelid": "UCX2Vhc0LIzSS9aMzhGFZ7PA",
+    "title": "DIY Four Monitor KVM Switch",
+    "title_detail": {
+        "type": "text/plain",
+        "language": None,
+        "base": "https://www.youtube.com/feeds/videos.xml?channel_id=UCX2Vhc0LIzSS9aMzhGFZ7PA",
+        "value": "DIY Four Monitor KVM Switch",
+    },
+    "links": [
+        {
+            "rel": "alternate",
+            "href": "https://www.youtube.com/watch?v=bNwWDifV8Bs",
+            "type": "text/html",
+        }
+    ],
+    "authors": [
+        {
+            "name": "Techno Dad Life",
+            "href": "https://www.youtube.com/channel/UCX2Vhc0LIzSS9aMzhGFZ7PA",
+        }
+    ],
+    "author_detail": {
+        "name": "Techno Dad Life",
+        "href": "https://www.youtube.com/channel/UCX2Vhc0LIzSS9aMzhGFZ7PA",
+    },
+    "href": "",
+    "author": "Techno Dad Life",
+    "published": "2021-05-03T17:01:50 00:00",
+    "published_parsed": time.struct_time(
+        tm_year=2021,
+        tm_mon=5,
+        tm_mday=3,
+        tm_hour=17,
+        tm_min=1,
+        tm_sec=50,
+        tm_wday=0,
+        tm_yday=123,
+        tm_isdst=0,
+    ),
+    "updated": "2021-05-04T09:41:28 00:00",
+    "updated_parsed": time.struct_time(
+        tm_year=2021,
+        tm_mon=5,
+        tm_mday=4,
+        tm_hour=9,
+        tm_min=41,
+        tm_sec=28,
+        tm_wday=1,
+        tm_yday=124,
+        tm_isdst=0,
+    ),
+    "media_content": [
+        {
+            "url": "https://www.youtube.com/v/bNwWDifV8Bs?version=3",
+            "type": "application/x-shockwave-flash",
+            "width": "640",
+            "height": "390",
+        }
+    ],
+    "media_thumbnail": [
+        {
+            "url": "https://i3.ytimg.com/vi/bNwWDifV8Bs/hqdefault.jpg",
+            "width": "480",
+            "height": "360",
+        }
+    ],
+    "summary": "Easy Quad Monitor KVM Switch\nSwitch with USB-C - https://amzn.to/3eep4kj\n4k x4 Output Switch - https://amzn.to/3vJFBTj\n\nBest $150 Mini Media Server : https://amzn.to/31EPTrl\nMy favorite Raspberry Pi Kit -  https://amzn.to/2uWBR6y\nUltimate Raspberry Pi case : https://amzn.to/3gxSurw\nMy VPN: http://bit.ly/2VQB0MW\nMy Usenetserver : http://bit.ly/2JGqKF4\n\nOpenmediavault forums - https://forum.openmediavault.org\nOMV 5 Videos : https://www.youtube.com/c/TechnoDadLife\n\n▶Support the Videos You Love : https://www.patreon.com/TechnoDadLife\nTo make a one time donation at: https://www.paypal.me/TechnoDadLife\n\n▶My gear on Amazon: https://amzn.to/3nc9G9o\n\nMain Camera- https://amzn.to/2RAtzbR\nWireless Mic - https://amzn.to/2XsgeYB\n\nCoffee mug - https://teespring.com/tdl-mug?tsmac=s... \n\nCheck out our new TDL Merch: https://teespring.com/stores/technoda...\n\nThanks for all your support!  These are affiliate links so they do provide some support to the channel, so I can more videos!",
+    "summary_detail": {
+        "type": "text/html",
+        "language": None,
+        "base": "https://www.youtube.com/feeds/videos.xml?channel_id=UCX2Vhc0LIzSS9aMzhGFZ7PA",
+        "value": "Easy Quad Monitor KVM Switch\nSwitch with USB-C - https://amzn.to/3eep4kj\n4k x4 Output Switch - https://amzn.to/3vJFBTj\n\nBest $150 Mini Media Server : https://amzn.to/31EPTrl\nMy favorite Raspberry Pi Kit -  https://amzn.to/2uWBR6y\nUltimate Raspberry Pi case : https://amzn.to/3gxSurw\nMy VPN: http://bit.ly/2VQB0MW\nMy Usenetserver : http://bit.ly/2JGqKF4\n\nOpenmediavault forums - https://forum.openmediavault.org\nOMV 5 Videos : https://www.youtube.com/c/TechnoDadLife\n\n▶Support the Videos You Love : https://www.patreon.com/TechnoDadLife\nTo make a one time donation at: https://www.paypal.me/TechnoDadLife\n\n▶My gear on Amazon: https://amzn.to/3nc9G9o\n\nMain Camera- https://amzn.to/2RAtzbR\nWireless Mic - https://amzn.to/2XsgeYB\n\nCoffee mug - https://teespring.com/tdl-mug?tsmac=s... \n\nCheck out our new TDL Merch: https://teespring.com/stores/technoda...\n\nThanks for all your support!  These are affiliate links so they do provide some support to the channel, so I can more videos!",
+    },
+    "media_starrating": {"count": "71", "average": "4.94", "min": "1", "max": "5"},
+    "media_statistics": {"views": "1015"},
+    "media_community": "",
+}
+```
--- youtube.py
+++ youtube.py
@@ -13,109 +13,113 @@ import feedparser
 
 class Youtube:
 
-    links = {}
+  links = {}
 
-    dpPath = "./youtube.db"
-    configPath = "./youtube.ini"
-    channelUrl = "https://www.youtube.com/channel/"
-    rssUrl = "https://www.youtube.com/feeds/videos.xml?channel_id="
+  dpPath = "./youtube.db"
+  configPath = "./youtube.ini"
+  channelUrl = "https://www.youtube.com/channel/"
+  rssUrl = "https://www.youtube.com/feeds/videos.xml?channel_id="
 
-    def __init__(self):
-        # self.config = configparser.ConfigParser()
-        # self.config.read(self.configPath)
-        logging.basicConfig()
-        self.logger = logging.getLogger(__name__)
-        self.logger.setLevel(logging.DEBUG)
+  def __init__(self):
+    # self.config = configparser.ConfigParser()
+    # self.config.read(self.configPath)
+    logging.basicConfig()
+    self.logger = logging.getLogger(__name__)
+    self.logger.setLevel(logging.DEBUG)
 
-        self.logger.info("Application Starting")
+    self.logger.info("Application Starting")
 
-        self.vaultClient = hvac.Client(
-            url="http://192.168.73.20:8200",
-            token="s.tLWbbS9mBlEcDedkystiYG8P"
-        )
+    self.vaultClient = hvac.Client(
+      url="http://192.168.73.20:8200",
+      token="s.tLWbbS9mBlEcDedkystiYG8P"
+    )
 
-        self.channel = self.vaultClient.secrets.kv.read_secret_version(path="youtube")["data"]["data"]["channel"]
-        username = self.vaultClient.secrets.kv.read_secret_version(path="youtube")["data"]["data"]["db-username"]
-        password = self.vaultClient.secrets.kv.read_secret_version(path="youtube")["data"]["data"]["db-password"]
+    self.channel = self.vaultClient.secrets.kv.read_secret_version(path="youtube")["data"]["data"]["channel"]
+    username = self.vaultClient.secrets.kv.read_secret_version(path="youtube")["data"]["data"]["db-username"]
+    password = self.vaultClient.secrets.kv.read_secret_version(path="youtube")["data"]["data"]["db-password"]
 
-        client = pymongo.MongoClient(
-            "mongodb://192.168.73.20:27017",
-            username = username,
-            password = password
-        )
-        nestDB = client["nestdb"]
-        self.youtubeDB = nestDB["youtube"]
+    client = pymongo.MongoClient(
+      "mongodb://192.168.73.20:27017",
+      username = username,
+      password = password
+    )
+    nestDB = client["nestdb"]
+    self.youtubeDB = nestDB["youtube"]
 
-        response = self.youtubeDB.find({}, {"_id":0, "category":1})
+    response = self.youtubeDB.find({}, {"_id":0, "category":1})
 
-        category = {cat["category"] for cat in response if len(cat["category"]) > 1}
-        self.links = { cat:self.vaultClient.secrets.kv.read_secret_version(path="youtube")["data"]["data"][cat] for cat in category}
+    category = {cat["category"] for cat in response if len(cat["category"]) > 1}
+    self.links = { cat:self.vaultClient.secrets.kv.read_secret_version(path="youtube")["data"]["data"][cat] for cat in category}
 
-    
-    def getChannelAndMostRecent(self):
-        response = self.youtubeDB.find({},{"_id":0, "channelId":1, "mostRecentId":1})
-        data = {channel["channelId"]:channel["mostRecentId"] for channel in response}
-        return data
+  
+  def getChannelAndMostRecent(self):
+    response = self.youtubeDB.find({},{"_id":0, "channelId":1, "mostRecentId":1})
+    data = {channel["channelId"]:channel["mostRecentId"] for channel in response}
+    return data
 
-    def getNewVideosForSubWithRSS(self, channelId, recentVideos):
-        newsFeed = feedparser.parse(self.rssUrl + channelId)
-        newVideos = []
-        for vidNdx in range(len(newsFeed.entries)):
-            videoId = newsFeed.entries[vidNdx].yt_videoid
-            if videoId in recentVideos:
-                return newVideos
-            newVideos.append(videoId)
+  def getNewVideosForSubWithRSS(self, channelId, recentVideos):
+    newsFeed = feedparser.parse(self.rssUrl + channelId)
+    newVideos = []
+    recentVideosIds = [vid[0] for vid in recentVideos]
+    for vidNdx in range(len(newsFeed.entries)):
+      videoId = newsFeed.entries[vidNdx].yt_videoid
+      published = newsFeed.entries[vidNdx].published
+      title = newsFeed.entries[vidNdx].title
+      if videoId in recentVideosIds:
         return newVideos
-           
+      newVideos.append((videoId, published, title))
+    return newVideos
+          
 
-    def postInDiscord(self, newVideos, channelId):
-        self.logger.info("Posting videos")
-        
-        response = self.youtubeDB.find({"channelId": channelId},{"_id":0, "category":1})
-        category = response[0]["category"]
+  def postInDiscord(self, newVideos, channelId):
+    self.logger.info("Posting videos")
+    
+    response = self.youtubeDB.find({"channelId": channelId},{"_id":0, "category":1})
+    category = response[0]["category"]
 
-        if len(category) == 0: return
-        url = self.links[category]
+    if len(category) == 0: return
+    url = self.links[category]
 
-        for video in newVideos[::-1]:
-            data = {}
-            data["content"] = "https://www.youtube.com/watch?v=" + video
-            data["username"] = "YoutubeBot"
+    for video in newVideos[::-1]:
+      data = {}
+      data["content"] = "https://www.youtube.com/watch?v=" + video[0]
+      data["username"] = "YoutubeBot"
 
-            result = requests.post(url, data=json.dumps(data), headers={
-                                "Content-Type": "application/json"})
-                            
-            time.sleep(2)
+      result = requests.post(url, data=json.dumps(data), headers={
+                          "Content-Type": "application/json"})
+                      
+      time.sleep(2)
 
-    def updateMostRecent(self, newVideo, channelId): 
+  def updateMostRecent(self, newVideo, channelId): 
+    response = self.youtubeDB.update_one({"channelId": channelId},{ "$set": { "mostRecentId": newVideo } })
 
-        response = self.youtubeDB.update_one({"channelId": channelId},{ "$set": { "mostRecentId": newVideo } })
-
-    def getMostRecentTen(self, channelId):
-        newsFeed = feedparser.parse(self.rssUrl + channelId)
-        videoIds = []
-        for vidNdx in range(len(newsFeed.entries)):
-            videoId = newsFeed.entries[vidNdx].yt_videoid
-            if len(videoIds) == 10:
-                return videoIds
-            videoIds.append(videoId)
+  def getMostRecentTen(self, channelId):
+    newsFeed = feedparser.parse(self.rssUrl + channelId)
+    videoIds = []
+    for vidNdx in range(len(newsFeed.entries)):
+      videoId = newsFeed.entries[vidNdx].yt_videoid
+      published = newsFeed.entries[vidNdx].published
+      title = newsFeed.entries[vidNdx].title
+      if len(videoIds) == 10:
         return videoIds
-        
+      videoIds.append((videoId, published, title))
+    return videoIds
+      
 
-    def run(self):
-        while True:
-            self.logger.info("Starting: " + str(datetime.now()))
-            
-            subInfo = self.getChannelAndMostRecent()
-            for subId in subInfo:
-                self.logger.info("Finding new videos for: " + subId)
-                newVideos = self.getNewVideosForSubWithRSS(subId, subInfo[subId])
-                if len(newVideos) > 0:
-                    self.updateMostRecent(self.getMostRecentTen(subId), subId)
-                    self.postInDiscord(newVideos, subId)
-            
-            self.logger.info("Sleeping: " + str(datetime.now()))
-            time.sleep(1800)
+  def run(self):
+    while True:
+      self.logger.info("Starting: " + str(datetime.now()))
+      
+      subInfo = self.getChannelAndMostRecent()
+      for subId in subInfo:
+        self.logger.info("Finding new videos for: " + subId)
+        newVideos = self.getNewVideosForSubWithRSS(subId, subInfo[subId])
+        if len(newVideos) > 0:
+          self.updateMostRecent(self.getMostRecentTen(subId), subId)
+          self.postInDiscord(newVideos, subId)
+      
+      self.logger.info("Sleeping: " + str(datetime.now()))
+      time.sleep(1800)
 
 if __name__ == "__main__":
     yt = Youtube()
